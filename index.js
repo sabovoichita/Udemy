2ed4d17f61b40d32b04b3defce7eb12bbeb1843a
Voichita Maria <sabo_voichita@yahoo.com>
2023-12-12 17:16:00 +0100

    improve events and add showPage(id)

--- index.js
+++ index.js
@@ -8,40 +8,52 @@ function show(id) {
   document.getElementById(id).style.display = "block";
 }
 
-function showHomePage() {
+function showPage(id) {
   hide(activePage);
-  show("home");
-  activePage = "home";
+  show(id);
+  activePage = id;
 }
 
-function showSkillsPage() {
-  hide(activePage);
-  show("skills");
-  activePage = "skills";
-}
-function showProjectsPage() {
-  hide(activePage);
-  show("projects");
-  activePage = "projects";
-}
+showPage(activePage);
 
-function showLanguagesPage() {
-  hide(activePage);
-  show("languages");
-  activePage = "languages";
-}
+document.querySelector("#top-menu-bar").addEventListener("click", function (e) {
+  var id = e.target.dataset.page;
+  console.info("click on menu-bar", id);
+  showPage(id);
+});
+
+// function showHomePage() {
+//   hide(activePage);
+//   show("home");
+//   activePage = "home";
+// }
+
+// function showSkillsPage() {
+//   hide(activePage);
+//   show("skills");
+//   activePage = "skills";
+// }
+// function showProjectsPage() {
+//   hide(activePage);
+//   show("projects");
+//   activePage = "projects";
+// }
+
+// function showLanguagesPage() {
+//   hide(activePage);
+//   show("languages");
+//   activePage = "languages";
+// }
 
 // showSkillsPage();
-showHomePage();
+// var homeLink = document.querySelectorAll("#top-menu-bar a")[0];
+// homeLink.addEventListener("click", showHomePage);
 
-var homeLink = document.querySelectorAll("#top-menu-bar a")[0];
-homeLink.addEventListener("click", showHomePage);
+// var skillsLink = document.querySelectorAll("#top-menu-bar a")[1];
+// skillsLink.addEventListener("click", showSkillsPage);
 
-var skillsLink = document.querySelectorAll("#top-menu-bar a")[1];
-skillsLink.addEventListener("click", showSkillsPage);
+// var projectsLink = document.querySelectorAll("#top-menu-bar a")[2];
+// projectsLink.addEventListener("click", showProjectsPage);
 
-var projectsLink = document.querySelectorAll("#top-menu-bar a")[2];
-projectsLink.addEventListener("click", showProjectsPage);
-
-var languagesLink = document.querySelectorAll("#top-menu-bar a")[3];
-languagesLink.addEventListener("click", showLanguagesPage);
+// var languagesLink = document.querySelectorAll("#top-menu-bar a")[3];
+// languagesLink.addEventListener("click", showLanguagesPage);
